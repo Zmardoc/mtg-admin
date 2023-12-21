df56c09128145d71aaf94489075df1e8ee1bfb0e
Martin Mol <martin.mol@kosik.cz>
2023-12-21 20:55:43 +0100

    v1.0.7

--- api/dist/database/cards.js
+++ api/dist/database/cards.js
@@ -5,7 +5,7 @@ const mongoClient_1 = require("./mongoClient");
 const CARDS_COLLECTION = 'cards';
 const cardCollection = (0, mongoClient_1.getCollection)(CARDS_COLLECTION);
 async function findCard(name, userId) {
-    return await cardCollection.findOne({ name, userId });
+    return await cardCollection.findOne({ 'frontFace.name': name, userId });
 }
 exports.findCard = findCard;
 async function insertCard(card) {
@@ -14,7 +14,7 @@ async function insertCard(card) {
 }
 exports.insertCard = insertCard;
 async function updateCard(card) {
-    await cardCollection.updateOne({ name: card.name }, { $set: card });
+    await cardCollection.updateOne({ 'frontFace.name': card.frontFace.name }, { $set: card });
     return true;
 }
 exports.updateCard = updateCard;
